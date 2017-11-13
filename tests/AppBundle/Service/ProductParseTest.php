873467425b16aa8c32475482c3a975d24df890f1
arunm123 <arun.kottooli@gmail.com>
2017-11-14 00:48:05 +0400

    updated Readme

--- tests/AppBundle/Service/ProductParseTest.php
+++ tests/AppBundle/Service/ProductParseTest.php
@@ -24,9 +24,26 @@ class ProductParserTest extends WebTestCase
         $document = $productParser->loadXML('http://pf.tradetracker.net/?aid=1&type=xml&encoding=utf-8&fid=251713&categoryType=2&additionalType=2&limit=10');
         $products = $productParser->parseDocument($document);
 
-         $this->assertCount(10, $products);
+        $this->assertCount(10, $products);
     }
 
+    public function testparseDocumentAttribute()
+    {
+        $client = static::createClient();
 
+        $productParser = $this->container->get(ProductParser::class);
+        $document = $productParser->loadXML('http://pf.tradetracker.net/?aid=1&type=xml&encoding=utf-8&fid=251713&categoryType=2&additionalType=2&limit=1');
+        $products = $productParser->parseDocument($document);
+
+        $this->assertCount(1, $products);
+
+        $this->assertAttributeNotEmpty('name', $products[0]); 
+        $this->assertAttributeNotEmpty('description', $products[0]); 
+        $this->assertAttributeNotEmpty('imageURL', $products[0]);
+        $this->assertAttributeNotEmpty('productURL', $products[0]);  
+        $this->assertAttributeNotEmpty('price', $products[0]); 
+        $this->assertAttributeNotEmpty('currency', $products[0]); 
+
+    }
 
 }
